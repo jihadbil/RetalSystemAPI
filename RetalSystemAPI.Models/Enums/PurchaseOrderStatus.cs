421d0260a7cdd94f1b82efd2f72536705644b4cc
Masoud <masoudbilkhair99@gmail.com>
2026-08-10 17:09:00 +0200

    تعديل ال enum الخاص بحالة الطلبية

---
+++ RetalSystemAPI.Models/Enums/PurchaseOrderStatus.cs
@@ -0,0 +1,10 @@
+namespace RetalSystemAPI.Models.Enums;
+
+public enum PurchaseOrderStatus
+{
+    Draft = 1,
+    Confirmed = 2,
+    PartiallyReceived = 3,
+    Received = 4,
+    Cancelled = 5
+}
