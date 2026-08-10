b398704b622c795857650b6e1b29c3067f7e84cb
Masoud <masoudbilkhair99@gmail.com>
2026-08-10 17:08:36 +0200

    انشاء النمادج الخاصة بالموردين

--- RetalSystemAPI.Models/Suppliers/Supplier.cs
+++ RetalSystemAPI.Models/Suppliers/Supplier.cs
@@ -3,13 +3,23 @@ using RetalSystemAPI.Models.Purchase;
 using RetalSystemAPI.Models.Suppliers;
 
 namespace RetalSystemAPI.Models.Suppliers;
-
+/// <summary>
+/// يمتل الموردين الدين يتعامل معهم صاحب المحل
+/// </summary>
 public class Supplier : BaseEntity
 {
+    /// <summary>
+    /// اسم المورد
+    /// </summary>
     public string Name { get; set; } = null!;
-        public string? Address { get; set; }
-
-       public decimal OpeningBalance { get; set; }
+    /// <summary>
+    /// عنوان المورد
+    /// </summary>
+    public string? Address { get; set; }
+    /// <summary>
+    /// الرصيد المبدئي للمورد عند بداية التعامل معه
+    /// </summary>
+    public decimal OpeningBalance { get; set; }
 
     public ICollection<SupplierPhone> SupplierPhones { get; set; } = new List<SupplierPhone>();
 
--- RetalSystemAPI.Models/Suppliers/SupplierPhone.cs
+++ RetalSystemAPI.Models/Suppliers/SupplierPhone.cs
@@ -6,16 +6,24 @@ using RetalSystemAPI.Models.Common;
 
 
 namespace RetalSystemAPI.Models.Suppliers;
-
+/// <summary>
+/// يمتل ارقام الهواتف الخصة بالمورد و اسماء اصحابها
+/// </summary>
 public class SupplierPhone:BaseEntity
 {
+    /// <summary>
+    /// معرف المورد
+    /// </summary>
     public Guid SupplierId { get; set; }
+        public Supplier Supplier { get; set; } 
 
-    public Supplier Supplier { get; set; } 
-
-
+    /// <summary>
+    /// رقم الهاتف
+    /// </summary>
     public string PhoneNumber { get; set; } = null!;
-
+    /// <summary>
+    /// اسم صاحب الهاتف
+    /// </summary>
     public string Name { get; set; }
 
 
