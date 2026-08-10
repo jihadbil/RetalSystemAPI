623e319e565e7f620a0aa49602443d00a5c80522
Masoud <masoudbilkhair99@gmail.com>
2026-08-10 17:02:05 +0200

    اضافة التنقل الخاص بالطلبيات و المخازن

--- RetalSystemAPI.Models/Branchs/Branch.cs
+++ RetalSystemAPI.Models/Branchs/Branch.cs
@@ -1,5 +1,8 @@
 using RetalSystemAPI.Models.Branchs;
 using RetalSystemAPI.Models.Common;
+using RetalSystemAPI.Models.Purchase;
+using RetalSystemAPI.Models.Warehouses;
+
 
 namespace RetalSystemAPI.Models.Branchs;
 /// <summary>
@@ -24,5 +27,8 @@ public class Branch : BaseEntity
     public ICollection<BranchPhone> BranchPhones { get; set; } = new List<BranchPhone>();
 
     public ICollection<ApplicationUser> ApplicationUser { get; set; }=new List<ApplicationUser>();
+    public ICollection<Warehouse> Warehouses { get; set; } = new List<Warehouse>(); 
+
+    public ICollection<PurchaseOrder> PurchaseOrders { get; set; } = new List<PurchaseOrder>();
 
 }
